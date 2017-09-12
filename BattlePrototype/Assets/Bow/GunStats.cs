819b43d125fa36682d64852e4c249bd13a8f3ba2
James Lambert <lambertjamesd@gmail.com>
2017-09-11 22:17:24 -0600

    Added shotgun

---
+++ BattlePrototype/Assets/Bow/GunStats.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System.IO;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif
+
+public enum AmmoType
+{
+    Colt45,
+    _44Magnum,
+    ShotgunShell,
+}
+
+public class GunStats : ScriptableObject
+{
+    public string gunName;
+    public float speed;
+    public int capacity;
+    public float reloadDelay;
+    public float reloadBulletDuration;
+    public ReloadAnimation reloadAnimation;
+    public Sprite smallIcon;
+    public Sprite fullSize;
+    public Projectile round;
+    public AmmoType type = AmmoType.Colt45;
+
+
+#if UNITY_EDITOR
+    [MenuItem("Assets/Create/Gun")]
+    static void CreateFont()
+    {
+        GunStats gunStats = ScriptableObject.CreateInstance<GunStats>();
+
+        string path = AssetDatabase.GetAssetPath(Selection.activeObject);
+        if (path == "")
+        {
+
+        }
+        else if (Path.GetExtension(path) != "")
+        {
+            path = path.Replace(Path.GetFileName(AssetDatabase.GetAssetPath(Selection.activeObject)), "");
+        }
+
+        string assetPathAndName = AssetDatabase.GenerateUniqueAssetPath(path + "/New Gun.asset");
+
+        AssetDatabase.CreateAsset(gunStats, assetPathAndName);
+        AssetDatabase.SaveAssets();
+        EditorUtility.FocusProjectWindow();
+        Selection.activeObject = gunStats;
+    }
+#endif
+
+    public int GetShotsLeft(VariableStore store)
+    {
+        return store.GetInt(gunName + "_shots");
+    }
+
+    public void SetShotsLeft(VariableStore store, int value)
+    {
+        store.SetInt(gunName + "_shots", value);
+    }
+}
--- BattlePrototype/Assets/Story/Player/Player.ink
+++ BattlePrototype/Assets/Story/Player/Player.ink
@@ -1,8 +1,13 @@
 VAR player_has_Colt45 = false
+
 VAR player_Colt45_count = 12
 VAR player_colt45_pistol_shots = 6
+
 VAR player__44Magnum_count = 12
 VAR player_winchester_rifle_shots = 4
+
+VAR player_shotgun_shots = 2
+VAR player_ShotgunShell_count = 8
 
 VAR player_money = 100
 VAR player_bank_money = 1000
